2ed3b592258f5fe9f39dc863fe67f3e8c0fbbe75
Ricardo Fuzeto <ricardofuzeto@gmail.com>
2019-08-13 17:53:08 -0300

    Added Docker files to run app along with MongoDB

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ app.use(express.json());
 
 app.get('/alert/:email/:query', (req, res) => {
   const alert = { email: req.params.email, query: req.params.query };
+  console.log(`GET request -> email=${alert.email}, query="${query}"`);
   getAlert(alert, (_, data) => res.send(data));
 });
 
 app.post('/alert', (req, res) => {
   const alert = req.body;
+  console.log(`POST request -> email=${alert.email}, query="${query}", frequency=${alert.frequency}`);
   updateAlert(alert, (err, newData) => {
     if (err) throw err;
     res.send(newData);
@@ -19,3 +21,4 @@ app.post('/alert', (req, res) => {
 });
 
 app.listen(3000);
+console.log('App is up, listening on port 3000');
