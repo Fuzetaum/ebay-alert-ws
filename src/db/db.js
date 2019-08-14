8a93e2b205a1d7f8f77cc8fce4970f5359976c04
Ricardo Fuzeto <ricardofuzeto@gmail.com>
2019-08-14 17:52:01 -0300

    Fixed some minor database issues

--- src/db/db.js
+++ src/db/db.js
@@ -7,30 +7,41 @@ const client = new MongoClient(new Server(DB_HOST, DB_PORT), {
   useUnifiedTopology: true,
 });
 
-const getAlert = (alert, cb) => {
-  client.connect((connectError, mongoClient) => {
-    if (connectError) throw connectError;
-    const db = mongoClient.db(DB_NAME);
-    db.collection('alerts').findOne({ email: alert.email, query: alert.query }, (err, result) => {
-      cb(err, result);
-      mongoClient.close();
-    });
+let db;
+
+client.connect((connectError, mongoClient) => {
+  if (connectError) throw connectError;
+  db = mongoClient.db(DB_NAME);
+});
+
+const createAlert = (alert, cb) => {
+  db.collection('alerts').findOne({ email: alert.email, query: alert.query }, (findError, result) => {
+    if (findError) throw findError;
+    if (!result) {
+      db.collection('alerts').insertOne(alert, (err, result) => {
+        cb(err, result);
+      });
+    } else {
+      cb(null, { message: 'Alert configuration already exists' });
+    }
+  });
+};
+
+const getAlerts = (cb) => {
+  db.collection('alerts').find({}).toArray((err, result) => {
+    cb(err, result);
   });
 };
 
 const updateAlert = (alert, cb) => {
-  client.connect((connectError, mongoClient) => {
-    if (connectError) throw connectError;
-    const db = mongoClient.db(DB_NAME);
-    db.collection('alerts').updateOne({ email: alert.email, query: alert.query },
-      { $set: { frequency: alert.frequency } }, (err, result) => {
-        cb(err, result);
-        mongoClient.close();
-      });
-  });
+  db.collection('alerts').updateOne({ email: alert.email, query: alert.query },
+    { $set: { frequency: alert.frequency } }, (err, result) => {
+      cb(err, result);
+    });
 };
 
 module.exports = {
-  getAlert,
+  createAlert,
+  getAlerts,
   updateAlert,
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 require('dotenv').config();
 const express = require('express');
-const { getAlert, updateAlert } = require('./db/db');
+const { createAlert, getAlerts, updateAlert } = require('./db/db');
 
 const app = express();
 app.use(express.json());
 
-app.get('/alert/:email/:query', (req, res) => {
-  const alert = { email: req.params.email, query: req.params.query };
-  console.log(`GET request -> email=${alert.email}, query="${query}"`);
-  getAlert(alert, (_, data) => res.send(data));
+app.get('/alerts', (req, res) => {
+  console.log(`GET request`);
+  getAlerts((_, data) => res.send(data));
+});
+
+app.patch('/alert', (req, res) => {
+  const alert = req.body;
+  console.log(`PATCH request -> email=${alert.email}, query="${alert.query}", frequency=${alert.frequency}`);
+  updateAlert(alert, (err, newData) => {
+    if (err) throw err;
+    res.send(newData);
+  });
 });
 
 app.post('/alert', (req, res) => {
   const alert = req.body;
-  console.log(`POST request -> email=${alert.email}, query="${query}", frequency=${alert.frequency}`);
-  updateAlert(alert, (err, newData) => {
+  console.log(`POST request -> email=${alert.email}, query="${alert.query}", frequency=${alert.frequency}`);
+  createAlert(alert, (err, newData) => {
     if (err) throw err;
     res.send(newData);
   });
